40dc8d4023d62698996dedd115bea1b1880b6496
Allen <alleneben@gmail.com>
2020-08-20 18:51:43 -0400

    change

---
+++ precache-manifest.54141cb79f39fb2d43b51d9afc7efeaa.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = [
+  {
+    "revision": "3377707ba00a4d4ffa0c68e868221dd2",
+    "url": "/static/media/bis_ocr_nya.3377707b.jpg"
+  },
+  {
+    "revision": "de0b9deffe5baf03c668",
+    "url": "/static/css/main.2531915d.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "3a56c4fa59a4076dc44ca8a2cb97bb0c",
+    "url": "/static/media/vote.3a56c4fa.jpg"
+  },
+  {
+    "revision": "27e5bacfb7b274117240",
+    "url": "/static/js/2.27e5bacf.chunk.js"
+  },
+  {
+    "revision": "eb6be414b75c412c13eeee2d2e8390cb",
+    "url": "/static/media/react-logo.eb6be414.png"
+  },
+  {
+    "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
+    "url": "/static/media/nucleo.f0b489a5.woff"
+  },
+  {
+    "revision": "5987dd12fea78ce5f97ae601b08ec03c",
+    "url": "/static/media/nucleo.5987dd12.woff2"
+  },
+  {
+    "revision": "03ef1918e505c3e3471f9369ef7a638f",
+    "url": "/static/media/nucleo.03ef1918.eot"
+  },
+  {
+    "revision": "b17a118e13e53558658b681a0ebdad82",
+    "url": "/static/media/nucleo.b17a118e.ttf"
+  },
+  {
+    "revision": "bd6820f1a4015e9645e470ea8e7b1501",
+    "url": "/static/media/anime3.bd6820f1.png"
+  },
+  {
+    "revision": "34dde3245ca08d21f962b47c997b792c",
+    "url": "/static/media/ado_sam_kwa.34dde324.jpg"
+  },
+  {
+    "revision": "f4c89b825c7019d6e337fcec188a204a",
+    "url": "/static/media/ado_sam_kwa_o.f4c89b82.jpg"
+  },
+  {
+    "revision": "8810baf1ce158abf80f8e4619e49f2f8",
+    "url": "/static/media/adu_pok_god.8810baf1.jpg"
+  },
+  {
+    "revision": "77824bdfe61110968a2aecde27e0e995",
+    "url": "/static/media/ang_ach_nya.77824bdf.jpg"
+  },
+  {
+    "revision": "8a4be824435e4f2dcfcbef56715a159b",
+    "url": "/static/media/ang_ach_nya_o.8a4be824.jpg"
+  },
+  {
+    "revision": "4396680ba5cabf849d84d9f34e28cadc",
+    "url": "/static/media/ant_fran.4396680b.jpg"
+  },
+  {
+    "revision": "834451102b657ac20d86d7e3bc9f16e8",
+    "url": "/static/media/ary_ben_ofo.83445110.jpg"
+  },
+  {
+    "revision": "a4dcd4e1c9d12f73ca5038768f30d04b",
+    "url": "/static/media/ary_ben_ofo_o.a4dcd4e1.jpg"
+  },
+  {
+    "revision": "de0b9deffe5baf03c668",
+    "url": "/static/js/main.de0b9def.chunk.js"
+  },
+  {
+    "revision": "e4fe1a0b6ecae169c5ce871116591e94",
+    "url": "/static/media/bis_ocr_nya_o.e4fe1a0b.jpg"
+  },
+  {
+    "revision": "ad26bb0f84b89d2c8a01ec083419b50b",
+    "url": "/static/media/den_att_asa.ad26bb0f.jpg"
+  },
+  {
+    "revision": "537f6a6603d27889a6bf46d8938ac5af",
+    "url": "/static/media/ess_mat.537f6a66.jpg"
+  },
+  {
+    "revision": "ed3ef1af849c91c6c9966839ae1bbf97",
+    "url": "/static/media/lou_ank.ed3ef1af.jpg"
+  },
+  {
+    "revision": "9d9f79528e0db4d1ee103906dad1dba4",
+    "url": "/static/media/lou_ank_o.9d9f7952.jpg"
+  },
+  {
+    "revision": "2542af9b28e63e2f510efeab2b1d9ee5",
+    "url": "/static/media/mav_ofr_men.2542af9b.jpg"
+  },
+  {
+    "revision": "44518418b819d868543798dfb5c323a1",
+    "url": "/static/media/mav_ofr_men_o.44518418.jpg"
+  },
+  {
+    "revision": "d736da70874540414b501438f8085b36",
+    "url": "/static/media/men_k_fos.d736da70.jpg"
+  },
+  {
+    "revision": "3059a3ac02b5f4e86b8046aed59a7e0c",
+    "url": "/static/media/men_k_fos_o.3059a3ac.jpg"
+  },
+  {
+    "revision": "b4c97926e0dcb23ce76add726bfe6402",
+    "url": "/static/media/men_mar_mac.b4c97926.jpg"
+  },
+  {
+    "revision": "4eb58e1e01ca63170a7d7743adbc2a7f",
+    "url": "/static/media/nat_gya_yaw.4eb58e1e.jpg"
+  },
+  {
+    "revision": "3044d780b3d5877f836dd49bc2f638a6",
+    "url": "/static/media/nke_dan.3044d780.jpg"
+  },
+  {
+    "revision": "6076eb9e0dfce1a7fabada7f689a577f",
+    "url": "/static/media/rob_dan_sub.6076eb9e.jpg"
+  },
+  {
+    "revision": "d25e24b6f419f2bb279ed82f47567607",
+    "url": "/static/media/sample.d25e24b6.png"
+  },
+  {
+    "revision": "6c38e4abf35b6a0c424ea93443fb7d1d",
+    "url": "/static/media/tet_nar_chr.6c38e4ab.jpg"
+  },
+  {
+    "revision": "b294ebdb0f54b0887003af6aa3ac8a32",
+    "url": "/static/media/zak_nam.b294ebdb.jpg"
+  },
+  {
+    "revision": "d03996ea4a4dbc84eaa4007ea91f3072",
+    "url": "/static/media/undraw_voting_nvu7.d03996ea.svg"
+  },
+  {
+    "revision": "27e5bacfb7b274117240",
+    "url": "/static/css/2.24a6ebcd.chunk.css"
+  },
+  {
+    "revision": "e90b99023200c0be83b9f92d20c35e35",
+    "url": "/index.html"
+  }
+];
